a2b9d697b1e759a83e939e8c9509b5f9de736722
Mastersam <abadasamuelosp@gmail.com>
2019-11-29 16:43:37 -0800

    more tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7-alpine
+FROM python:3-alpine
 MAINTAINER Mastersam
 
 ENV PYTHONUNBUFFERED 1
@@ -12,5 +12,5 @@ RUN mkdir /app
 WORKDIR /app
 COPY ./app/ /app
 
-RUN adduser -D user
-USER user
+RUN adduser -D mastersam
+USER mastersam
--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from . import models
-
+from django.utils.translation import gettext as _
 
 # Register your models here.
 
@@ -9,6 +9,20 @@ from . import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['name', 'email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
